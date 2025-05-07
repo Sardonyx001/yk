f30c7dd527391cd975a846186aa962a2d1f73266
jam <lassouedjamel99@gmail.com>
2025-05-07 18:45:28 +0900

    faet: add boilerplate code

--- main.go
+++ main.go
@@ -11,7 +11,15 @@ import (
 
 var docStyle = lipgloss.NewStyle().Margin(1, 2)
 
+type status int
+
+const (
+	Ongoing status = iota
+	Done
+)
+
 type item struct {
+	status      status
 	title, desc string
 }
 
@@ -49,29 +57,29 @@ func (m model) View() string {
 
 func main() {
 	items := []list.Item{
-		item{title: "Raspberry Pi’s", desc: "I have ’em all over my house"},
-		item{title: "Nutella", desc: "It's good on toast"},
-		item{title: "Bitter melon", desc: "It cools you down"},
-		item{title: "Nice socks", desc: "And by that I mean socks without holes"},
-		item{title: "Eight hours of sleep", desc: "I had this once"},
-		item{title: "Cats", desc: "Usually"},
-		item{title: "Plantasia, the album", desc: "My plants love it too"},
-		item{title: "Pour over coffee", desc: "It takes forever to make though"},
-		item{title: "VR", desc: "Virtual reality...what is there to say?"},
-		item{title: "Noguchi Lamps", desc: "Such pleasing organic forms"},
-		item{title: "Linux", desc: "Pretty much the best OS"},
-		item{title: "Business school", desc: "Just kidding"},
-		item{title: "Pottery", desc: "Wet clay is a great feeling"},
-		item{title: "Shampoo", desc: "Nothing like clean hair"},
-		item{title: "Table tennis", desc: "It’s surprisingly exhausting"},
-		item{title: "Milk crates", desc: "Great for packing in your extra stuff"},
-		item{title: "Afternoon tea", desc: "Especially the tea sandwich part"},
-		item{title: "Stickers", desc: "The thicker the vinyl the better"},
-		item{title: "20° Weather", desc: "Celsius, not Fahrenheit"},
-		item{title: "Warm light", desc: "Like around 2700 Kelvin"},
-		item{title: "The vernal equinox", desc: "The autumnal equinox is pretty good too"},
-		item{title: "Gaffer’s tape", desc: "Basically sticky fabric"},
-		item{title: "Terrycloth", desc: "In other words, towel fabric"},
+		item{status: Ongoing, title: "Raspberry Pi’s", desc: "I have ’em all over my house"},
+		item{status: Ongoing, title: "Nutella", desc: "It's good on toast"},
+		item{status: Ongoing, title: "Bitter melon", desc: "It cools you down"},
+		item{status: Ongoing, title: "Nice socks", desc: "And by that I mean socks without holes"},
+		item{status: Ongoing, title: "Eight hours of sleep", desc: "I had this once"},
+		item{status: Ongoing, title: "Cats", desc: "Usually"},
+		item{status: Ongoing, title: "Plantasia, the album", desc: "My plants love it too"},
+		item{status: Ongoing, title: "Pour over coffee", desc: "It takes forever to make though"},
+		item{status: Ongoing, title: "VR", desc: "Virtual reality...what is there to say?"},
+		item{status: Ongoing, title: "Noguchi Lamps", desc: "Such pleasing organic forms"},
+		item{status: Ongoing, title: "Linux", desc: "Pretty much the best OS"},
+		item{status: Ongoing, title: "Business school", desc: "Just kidding"},
+		item{status: Ongoing, title: "Pottery", desc: "Wet clay is a great feeling"},
+		item{status: Ongoing, title: "Shampoo", desc: "Nothing like clean hair"},
+		item{status: Ongoing, title: "Table tennis", desc: "It’s surprisingly exhausting"},
+		item{status: Ongoing, title: "Milk crates", desc: "Great for packing in your extra stuff"},
+		item{status: Ongoing, title: "Afternoon tea", desc: "Especially the tea sandwich part"},
+		item{status: Ongoing, title: "Stickers", desc: "The thicker the vinyl the better"},
+		item{status: Ongoing, title: "20° Weather", desc: "Celsius, not Fahrenheit"},
+		item{status: Ongoing, title: "Warm light", desc: "Like around 2700 Kelvin"},
+		item{status: Ongoing, title: "The vernal equinox", desc: "The autumnal equinox is pretty good too"},
+		item{status: Ongoing, title: "Gaffer’s tape", desc: "Basically sticky fabric"},
+		item{status: Ongoing, title: "Terrycloth", desc: "In other words, towel fabric"},
 	}
 
 	m := model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)}
